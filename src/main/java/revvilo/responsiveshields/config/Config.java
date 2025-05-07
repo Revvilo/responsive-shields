7cc51e3abace4cb4f74ecf0e4f4a3e9e5cb43198
revvilo <revvilo@protonmail.com>
2025-05-07 21:27:37 +1200

    Non-working update to 1.21.5

--- src/main/java/revvilo/responsiveshields/config/Config.java
+++ src/main/java/revvilo/responsiveshields/config/Config.java
@@ -6,7 +6,7 @@ public final class Config {
     public static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
     public static final ModConfigSpec SPEC;
 
-    public static final ModConfigSpec.IntValue SHIELD_DELAY;
+    public static final ModConfigSpec.DoubleValue SHIELD_DELAY;
     public static final ModConfigSpec.BooleanValue IS_ENABLED;
 
     static {
@@ -15,7 +15,7 @@ public final class Config {
             "Setting to 5 is the same as vanilla.",
             "Setting to 0 makes shields capable of blocking instantly.",
             "The shield's 'Block' animation takes roughly 1 tick to complete, so setting to 1 is a good balance to prevent block-hitting."
-            ).defineInRange("raiseTime", 0, 0, Integer.MAX_VALUE);
+            ).defineInRange("raiseTime", 0, 0, Double.MAX_VALUE);
 
         IS_ENABLED = BUILDER.comment("Whether or not the mods effect is enabled.").define("enabled", true);
         
--- src/main/java/revvilo/responsiveshields/mixin/MixinTicksConst.java
+++ src/main/java/revvilo/responsiveshields/mixin/MixinTicksConst.java
@@ -1,17 +1,26 @@
 package revvilo.responsiveshields.mixin;
 
+import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-import net.minecraft.world.entity.LivingEntity;
 import revvilo.responsiveshields.config.Config;
 
-@Mixin(LivingEntity.class)
+@Mixin(Items.class)
 public class MixinTicksConst {
-    @ModifyConstant(method = "isBlocking", constant = @Constant(intValue = 5))
-    private int setShieldUseDelay(int constant) {
-        if(Config.IS_ENABLED.get()) return Config.SHIELD_DELAY.get();
+    @ModifyArg(
+            method = "registerItem(Ljava/lang/String;Ljava/util/function.Supplier;Lnet/minecraft/world/item/Item$Properties;)Lnet/minecraft/world/item/Item;",
+            at = @At(
+                    value = "NEW",
+                    target = "net/minecraft/world/item/BlocksAttacks"
+            ),
+            index = 0 // Index of the first argument (0.25F)
+    )
+    private float setShieldUseDelay(float constant) {
+        if(Config.IS_ENABLED.get()) return Config.SHIELD_DELAY.get().floatValue();
         return constant;
     }
 }
