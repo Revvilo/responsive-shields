5c508a27cb044b0995461361d7fa7b788f041c33
revvilo <revvilo@protonmail.com>
2026-05-03 20:20:02 +1200

    asd

--- src/main/java/revvilo/responsiveshields/mixin/MixinTicksConst.java
+++ src/main/java/revvilo/responsiveshields/mixin/MixinTicksConst.java
@@ -1,26 +1,25 @@
 package revvilo.responsiveshields.mixin;
 
-import net.minecraft.world.item.Items;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
 
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import revvilo.responsiveshields.config.Config;
 
-@Mixin(Items.class)
+@Mixin(LivingEntity.class)
 public class MixinTicksConst {
-    @ModifyArg(
-            method = "registerItem(Ljava/lang/String;Ljava/util/function.Supplier;Lnet/minecraft/world/item/Item$Properties;)Lnet/minecraft/world/item/Item;",
-            at = @At(
-                    value = "NEW",
-                    target = "net/minecraft/world/item/BlocksAttacks"
-            ),
-            index = 0 // Index of the first argument (0.25F)
-    )
-    private float setShieldUseDelay(float constant) {
-        if(Config.IS_ENABLED.get()) return Config.SHIELD_DELAY.get().floatValue();
-        return constant;
+    @Shadow protected ItemStack useItem;
+
+    @Inject(method = "getItemBlockingWith()Lnet/minecraft/world/item/ItemStack;",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;getUseDuration(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/LivingEntity;)I"), cancellable = true)
+    private void setShieldUseDelay(CallbackInfoReturnable<ItemStack> cir) {
+        if(Config.IS_ENABLED.get()) {
+            cir.setReturnValue(useItem);
+        }
     }
 }
+
+// net.minecraft.world.item.Item.getUseDuration
